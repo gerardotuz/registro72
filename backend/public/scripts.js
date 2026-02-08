284081c5b64e81ae90f41499acf2cae8d300c43c
gerardotuz <82822594+gerardotuz@users.noreply.github.com>
2026-02-07 19:47:43 -0500

    Update scripts.js

--- backend/public/scripts.js
+++ backend/public/scripts.js
@@ -4,7 +4,7 @@
 
 const BASE_URL = window.location.origin.includes("localhost")
   ? "http://localhost:3001"
-  : "https://registro214.onrender.com";
+  : "https://registro72.onrender.com";
 
 /* =========================
    FUNCION PARA MAYUSCULAS
